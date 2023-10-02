ca58135748b636580e096b465c76c7aba69b0ffc
Lindsey Steiner <105761115+Razetto@users.noreply.github.com>
2023-10-02 16:46:44 -0400

    Initial home page commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// JavaScript to Toggle the Menu (Show/Hide)
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
