79744f7aa44ac4d1cd63cfcc3fc63b6249d68ad7
Lindsey Steiner <105761115+Razetto@users.noreply.github.com>
2023-10-03 13:06:20 -0400

    update to navigation

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 var navLinks = document.getElementById("navLinks");
 
 function showMenu() {
-    navLinks.style.right = "0";
+    navLinks.style.width = "200px";
 }
 
 function hideMenu() {
-    navLinks.style.right = "-200px";
+    navLinks.style.width = "0";
 }
