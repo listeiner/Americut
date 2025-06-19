03bd8e600985d31fc1526b20594def5896980f0d
Max Rigout <71095568+maxrigout@users.noreply.github.com>
2025-06-18 20:38:46 -0400

    added favicon, and button to hide email

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 
 // JavaScript to Toggle the Menu (Show/Hide)
 
+const bee = 'YW1lcmljdXRAd2luZHN0cmVhbS5uZXQ=';
+
+function decode64(input) {
+    var keyStr = "ABCDEFGHIJKLMNOP" +
+	               "QRSTUVWXYZabcdef" +
+	               "ghijklmnopqrstuv" +
+	               "wxyz0123456789+/" +
+	               "=";
+   var output = "";
+   var chr1, chr2, chr3 = "";
+   var enc1, enc2, enc3, enc4 = "";
+   var i = 0;
+   // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+   var base64test = /[^A-Za-z0-9\+\/\=]/g;
+   if (base64test.exec(input)) {
+      alert("There were invalid base64 characters in the input text.\n" +
+            "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+            "Expect errors in decoding.");
+   }
+   input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+   do {
+      enc1 = keyStr.indexOf(input.charAt(i++));
+      enc2 = keyStr.indexOf(input.charAt(i++));
+      enc3 = keyStr.indexOf(input.charAt(i++));
+      enc4 = keyStr.indexOf(input.charAt(i++));
+      chr1 = (enc1 << 2) | (enc2 >> 4);
+      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+      chr3 = ((enc3 & 3) << 6) | enc4;
+      output = output + String.fromCharCode(chr1);
+      if (enc3 != 64) {
+         output = output + String.fromCharCode(chr2);
+      }
+      if (enc4 != 64) {
+         output = output + String.fromCharCode(chr3);
+      }
+      chr1 = chr2 = chr3 = "";
+      enc1 = enc2 = enc3 = enc4 = "";
+   } while (i < input.length);
+   return unescape(output);
+}
+
 var navLinks = document.getElementById("navLinks");
 
 function showMenu() {
@@ -9,6 +50,21 @@ function showMenu() {
 
 function hideMenu() {
     navLinks.style.width = "0";
+}
+
+function gbe() {
+    return decode64(bee);
+}
+
+function onClick(event) {
+    // const e = document.createElement("h5");
+    // e.innerHTML = gbe();
+    console.log(event);
+    // event.target.appendChild(e);
+    event.target.onclick = null;
+    event.target.style.display = "none";
+
+    document.querySelector("button + h5").innerHTML = gbe();
 }
 
 function isFormDataValid({name, email, subject, message}) {
@@ -46,14 +102,13 @@ if (submitBtn !== null) {
             return;
         }
 
-        // const emailToSendTo = "americut@windstream.net";
-        const emailToSendTo = "americut@windstream.net";
+        const to = gbe();
         const formData = new URLSearchParams();
         formData.set("body", message);
         formData.set("subject", subject);
         console.log(formData);
 
-        const link = `mailto:${emailToSendTo}?cc=${email}&${formData.toString()}`;
+        const link = `mailto:${to}?cc=${email}&${formData.toString()}`;
 
         console.log(link);
         
